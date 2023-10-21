7d42c3a1ff6b8a3451e0e7ba416b993b44494e79
Tobias Garcia <tobiasgarciaiacovelli@gmail.com>
2023-10-21 15:28:49 +0000

    Parte 2 lab 2

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,7 +1,3 @@
-class Monster < ApplicationRecord
-  validates :name, presence: true
-  validates :name, uniqueness: true
-
-  has_many :attacks
-  has_many :victims, through: :attacks
-end
+git add .
+git commit -m "Parte 1 lab 2"
+git push
--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,3 +1,6 @@
 class Victim < ApplicationRecord
   validates :name, presence: true
+
+  has_many :attacks, dependent: :destroy
+  has_many :monsters, through: :attacks, dependent: :destroy
 end
